cd9fc0408629c5fe9c050208e4c4b752ce403a6f
domtheducki <dominictrue10@gmail.com>
2026-07-07 17:31:43 -0400

    Update scoring formula in score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-18.71*Math.pow(rank-1, 0.69) + 350) *
+    let score = (-18.71*Math.pow(rank-1, 0.159) + 350) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
